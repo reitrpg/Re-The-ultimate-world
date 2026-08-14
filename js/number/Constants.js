9a09c85823ce6c439678b53ab051866234c6f92d
reitrpg <rei.you2.11@gmail.com>
2026-08-14 19:51:54 +0900

    js/number/Constants.js の作成

---
+++ js/number/Constants.js
@@ -0,0 +1,72 @@
+/**
+ * World Creator
+ * BigNumber Constants
+ */
+
+const BASE = 1000;
+
+const PRECISION = 6;
+
+const MIN_EXPONENT = 0;
+
+const MAX_EXPONENT = 1000000;
+
+const UNITS = [
+
+    "",
+
+    "K",
+
+    "M",
+
+    "B",
+
+    "T",
+
+    "Qa",
+
+    "Qi",
+
+    "Sx",
+
+    "Sp",
+
+    "Oc",
+
+    "No",
+
+    "Dc",
+
+    "Ud",
+
+    "Dd",
+
+    "Td",
+
+    "Qad",
+
+    "Qid",
+
+    "Sxd",
+
+    "Spd",
+
+    "Ocd",
+
+    "Nod"
+
+];
+
+export {
+
+    BASE,
+
+    PRECISION,
+
+    MIN_EXPONENT,
+
+    MAX_EXPONENT,
+
+    UNITS
+
+};
