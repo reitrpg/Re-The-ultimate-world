bf89b69aa4c2058cacf3cf958213ebee50b81149
reitrpg <rei.you2.11@gmail.com>
2026-08-15 23:43:32 +0900

    Manager.js の更新

--- js/world/Manager.js
+++ js/world/Manager.js
@@ -66,3 +66,121 @@ class WorldManager {
             index < 0 ||
             index >=
                 this.worlds.length
+        ) {
+
+            return false;
+
+        }
+
+        this.activeWorldIndex =
+            index;
+
+        eventBus.emit(
+            "world:update"
+        );
+
+        return true;
+
+    }
+
+    update(deltaTime) {
+
+        const world =
+            this.getActive();
+
+        if (!world) {
+
+            return;
+        }
+
+        world.update(
+            deltaTime
+        );
+
+        eventBus.emit(
+            "world:update"
+        );
+
+    }
+
+    clear() {
+
+        this.worlds = [];
+
+        this.activeWorldIndex = 0;
+
+        eventBus.emit(
+            "world:update"
+        );
+
+    }
+
+    toJSON() {
+
+        return {
+
+            worlds:
+                this.worlds.map(
+
+                    world =>
+
+                        world.toJSON()
+
+                ),
+
+            activeWorldIndex:
+                this.activeWorldIndex
+
+        };
+
+    }
+
+    load(data) {
+
+        if (!data) {
+
+            return;
+
+        }
+
+        this.worlds = [];
+
+        if (
+
+            Array.isArray(
+                data.worlds
+            )
+
+        ) {
+
+            data.worlds.forEach(
+
+                worldData => {
+
+                    const world =
+                        new World();
+
+                    world.load(
+                        worldData
+                    );
+
+                    this.worlds.push(
+                        world
+                    );
+
+                }
+
+            );
+
+        }
+
+        this.activeWorldIndex =
+
+            data.activeWorldIndex ||
+            0;
+
+    }
+
+}
+
+export default new WorldManager();
