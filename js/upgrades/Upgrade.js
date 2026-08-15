5e74ba017c94157fc8e69cc013cf2d530cb6b0e3
reitrpg <rei.you2.11@gmail.com>
2026-08-15 23:48:36 +0900

    js/upgrades/Upgrade.js の作成

---
+++ js/upgrades/Upgrade.js
@@ -0,0 +1,159 @@
+/**
+ * World Creator
+ * Upgrade
+ */
+
+import EPManager from "../ep/Manager.js";
+
+import BigNumber from "../number/BigNumber.js";
+
+class Upgrade {
+
+    constructor(
+
+        id = "",
+
+        name = "",
+
+        multiplier = 1,
+
+        cost = 0
+
+    ) {
+
+        this.id = id;
+
+        this.name = name;
+
+        this.level = 0;
+
+        this.multiplier = multiplier;
+
+        this.baseCost =
+
+            BigNumber.from(
+                cost
+            );
+
+    }
+
+    getCost() {
+
+        return this.baseCost.multiply(
+
+            Math.pow(
+
+                2,
+
+                this.level
+
+            )
+
+        );
+
+    }
+
+    canBuy() {
+
+        return EPManager.has(
+
+            this.getCost()
+
+        );
+
+    }
+
+    buy() {
+
+        if (
+
+            !this.canBuy()
+
+        ) {
+
+            return false;
+
+        }
+
+        EPManager.consume(
+
+            this.getCost()
+
+        );
+
+        this.level++;
+
+        return true;
+
+    }
+
+    getMultiplier() {
+
+        return Math.pow(
+
+            this.multiplier,
+
+            this.level
+
+        );
+
+    }
+
+    toJSON() {
+
+        return {
+
+            id:
+                this.id,
+
+            name:
+                this.name,
+
+            level:
+                this.level,
+
+            multiplier:
+                this.multiplier,
+
+            baseCost:
+
+                this.baseCost
+                    .toJSON()
+
+        };
+
+    }
+
+    load(data) {
+
+        if (!data) {
+
+            return;
+
+        }
+
+        this.id =
+            data.id;
+
+        this.name =
+            data.name;
+
+        this.level =
+            data.level || 0;
+
+        this.multiplier =
+            data.multiplier;
+
+        this.baseCost =
+
+            BigNumber.from(
+
+                data.baseCost
+
+            );
+
+    }
+
+}
+
+export default Upgrade;
