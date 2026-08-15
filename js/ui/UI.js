929798107605bad9c3d136d7cad0aeb95ff12492
reitrpg <rei.you2.11@gmail.com>
2026-08-15 23:37:11 +0900

    UI.js の更新

--- js/ui/UI.js
+++ js/ui/UI.js
@@ -1,14 +1,20 @@
 /**
  * World Creator
- * UI
+ * UI Manager
  */
 
 import eventBus from "../core/eventBus.js";
 
-import WorldManager from "../world/Manager.js";
-import ResourceManager from "../resource/Manager.js";
-import EPManager from "../ep/Manager.js";
-import SettingsManager from "../settings/Manager.js";
+import WorldUI from "./WorldUI.js";
+import ResearchUI from "./ResearchUI.js";
+import UpgradeUI from "./UpgradeUI.js";
+import ConverterUI from "./ConverterUI.js";
+import RebirthUI from "./RebirthUI.js";
+import SettingsUI from "./SettingsUI.js";
+import DebugUI from "./DebugUI.js";
+import SaveUI from "./SaveUI.js";
+import NotificationUI from "./NotificationUI.js";
+import ErrorUI from "./ErrorUI.js";
 
 class UI {
 
@@ -28,6 +34,26 @@ class UI {
 
         this.initialized = true;
 
+        WorldUI.initialize();
+
+        ResearchUI.initialize();
+
+        UpgradeUI.initialize();
+
+        ConverterUI.initialize();
+
+        RebirthUI.initialize();
+
+        SettingsUI.initialize();
+
+        DebugUI.initialize();
+
+        SaveUI.initialize();
+
+        NotificationUI.initialize();
+
+        ErrorUI.initialize();
+
         this.registerEvents();
 
         this.update();
@@ -40,187 +66,57 @@ class UI {
 
             "world:update",
 
-            "world:active",
-
             "resource:update",
-
-            "ep:update",
 
             "research:update",
 
-            "upgrade:update"
+            "upgrade:update",
+
+            "converter:update",
+
+            "rebirth:update",
+
+            "settings:update",
+
+            "debug:update"
 
         ];
 
-        for (const event of events) {
+        events.forEach(
 
-            eventBus.on(
+            event => {
 
-                event,
+                eventBus.on(
 
-                () => {
+                    event,
 
-                    this.update();
+                    () => {
 
-                }
+                        this.update();
 
-            );
+                    }
 
-        }
-
-    }
-
-    getElement(id) {
-
-        return document.getElementById(id);
-
-    }
-
-    format(value) {
-
-        if (
-            value === null ||
-            value === undefined
-        ) {
-
-            return "0";
-
-        }
-
-        if (
-            typeof value.toString ===
-            "function"
-        ) {
-
-            return value.toString();
-
-        }
-
-        return String(value);
-
-    }
-
-    updateWorld() {
-
-        const world =
-            WorldManager.getActive();
-
-        if (!world) {
-
-            return;
-
-        }
-
-        const worldName =
-            this.getElement(
-                "world-name"
-            );
-
-        const worldLevel =
-            this.getElement(
-                "world-level"
-            );
-
-        const worldRarity =
-            this.getElement(
-                "world-rarity"
-            );
-
-        if (worldName) {
-
-            worldName.textContent =
-                world.name;
-
-        }
-
-        if (worldLevel) {
-
-            worldLevel.textContent =
-                world.level;
-
-        }
-
-        if (worldRarity) {
-
-            worldRarity.textContent =
-                world.rarity;
-
-        }
-
-    }
-
-    updateResources() {
-
-        const container =
-            this.getElement(
-                "resource-list"
-            );
-
-        if (!container) {
-
-            return;
-
-        }
-
-        container.innerHTML = "";
-
-        const resources =
-            ResourceManager.getAll();
-
-        for (
-
-            const resource of
-
-            Object.values(
-                resources
-            )
-
-        ) {
-
-            const row =
-                document.createElement(
-                    "div"
                 );
 
-            row.textContent =
+            }
 
-                `${resource.name}: ${this.format(resource.amount)}`;
-
-            container.appendChild(
-                row
-            );
-
-        }
-
-    }
-
-    updateEP() {
-
-        const element =
-            this.getElement(
-                "ep-value"
-            );
-
-        if (!element) {
-
-            return;
-
-        }
-
-        element.textContent =
-            this.format(
-                EPManager.get()
-            );
+        );
 
     }
 
     update() {
 
-        this.updateWorld();
+        WorldUI.render();
 
-        this.updateResources();
+        ResearchUI.render();
 
-        this.updateEP();
+        UpgradeUI.render();
+
+        ConverterUI.render();
+
+        RebirthUI.render();
+
+        ErrorUI.render();
 
     }
 
