23922f2049cc53ec13993f2ad525f46ce2c9fb82
reitrpg <rei.you2.11@gmail.com>
2026-08-15 23:22:48 +0900

    main.js の更新

--- js/core/main.js
+++ js/core/main.js
@@ -6,14 +6,21 @@
 import SaveManager from "./save.js";
 import Game from "./game.js";
 
+import OfflineProgress from "../utils/OfflineProgress.js";
+
 import ResourceManager from "../resource/Manager.js";
 
 import WorldManager from "../world/Manager.js";
 
 import UI from "../ui/UI.js";
 import TabUI from "../ui/TabUI.js";
+import WorldUI from "../ui/WorldUI.js";
 import ResearchUI from "../ui/ResearchUI.js";
 import UpgradeUI from "../ui/UpgradeUI.js";
+import ConverterUI from "../ui/ConverterUI.js";
+import RebirthUI from "../ui/RebirthUI.js";
+import SettingsUI from "../ui/SettingsUI.js";
+import SaveUI from "../ui/SaveUI.js";
 import DebugUI from "../ui/DebugUI.js";
 
 function initializeResources() {
@@ -60,9 +67,19 @@ function initializeUI() {
 
     TabUI.initialize();
 
+    WorldUI.initialize();
+
     ResearchUI.initialize();
 
     UpgradeUI.initialize();
+
+    ConverterUI.initialize();
+
+    RebirthUI.initialize();
+
+    SettingsUI.initialize();
+
+    SaveUI.initialize();
 
     DebugUI.initialize();
 
@@ -82,6 +99,8 @@ function initializeGame() {
 
     }
 
+    OfflineProgress.calculate();
+
     initializeUI();
 
     SaveManager.startAutoSave();
@@ -89,6 +108,20 @@ function initializeGame() {
     Game.start();
 
 }
+
+window.addEventListener(
+
+    "beforeunload",
+
+    () => {
+
+        OfflineProgress.saveTimestamp();
+
+        SaveManager.save();
+
+    }
+
+);
 
 document.addEventListener(
 
