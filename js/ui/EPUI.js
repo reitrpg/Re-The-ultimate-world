e4d98a7c3fe86a4dc8fbbe578a3edb1dfd5874eb
reitrpg <rei.you2.11@gmail.com>
2026-09-24 10:19:00 +0900

    Fix World Creator public build and core integration

--- js/ui/EPUI.js
+++ js/ui/EPUI.js
@@ -8,69 +8,26 @@ import Formatter from "../utils/Formatter.js";
 import eventBus from "../core/eventBus.js";
 
 class EPUI {
-
     constructor() {
-
         this.initialized = false;
-
     }
 
     initialize() {
-
-        if (this.initialized) {
-
-            return;
-
-        }
-
+        if (this.initialized) return;
         this.initialized = true;
-
         this.registerEvents();
-
         this.render();
-
     }
 
     registerEvents() {
-
-        eventBus.on(
-
-            "ep:update",
-
-            () => {
-
-                this.render();
-
-            }
-
-        );
-
+        eventBus.on("ep:update", () => this.render());
     }
 
     render() {
-
-        const element =
-
-            document.getElementById(
-                "ep-value"
-            );
-
-        if (!element) {
-
-            return;
-
-        }
-
-        element.textContent =
-
-            Formatter.format(
-
-                EPManager.get()
-
-            );
-
+        const element = document.getElementById("ep-value");
+        if (!element) return;
+        element.textContent = Formatter.format(EPManager.get());
     }
-
 }
 
 export default new EPUI();
