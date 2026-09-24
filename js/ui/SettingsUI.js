994e99ae74feec24d386f42068594f3c4dc58b02
reitrpg <rei.you2.11@gmail.com>
2026-09-24 10:21:09 +0900

    Repair save offline and settings systems

--- js/ui/SettingsUI.js
+++ js/ui/SettingsUI.js
@@ -1,104 +1,20 @@
-/**
- * World Creator
- * Settings UI
- */
-
 import eventBus from "../core/eventBus.js";
 import SettingsManager from "../settings/Manager.js";
 import Game from "../core/game.js";
 import SaveManager from "../core/save.js";
-
-class SettingsUI {
-    constructor() {
-        this.initialized = false;
-    }
-
-    initialize() {
-        if (this.initialized) return;
-
-        this.initialized = true;
-        this.registerControls();
-        this.registerEvents();
-        this.render();
-    }
-
-    getElement(id) {
-        return document.getElementById(id);
-    }
-
-    registerControls() {
-        const numberFormat = this.getElement("settings-number-format");
-        if (numberFormat) {
-            numberFormat.addEventListener("change", event => {
-                SettingsManager.set("numberFormat", event.target.value);
-            });
-        }
-
-        const tickSpeed = this.getElement("settings-tick-speed");
-        if (tickSpeed) {
-            tickSpeed.addEventListener("change", event => {
-                if (!SettingsManager.set("tickSpeed", event.target.value)) {
-                    this.render();
-                    return;
-                }
-                Game.restart();
-            });
-        }
-
-        const autoSave = this.getElement("settings-autosave");
-        if (autoSave) {
-            autoSave.addEventListener("change", event => {
-                if (!SettingsManager.set("autoSaveInterval", event.target.value)) {
-                    this.render();
-                    return;
-                }
-                SaveManager.restartAutoSave();
-            });
-        }
-
-        const debugMode = this.getElement("settings-debug");
-        if (debugMode) {
-            debugMode.addEventListener("change", event => {
-                SettingsManager.set("debugMode", event.target.checked);
-            });
-        }
-
-        const speedRunMode = this.getElement("settings-speedrun");
-        if (speedRunMode) {
-            speedRunMode.addEventListener("change", event => {
-                SettingsManager.set("speedRunMode", event.target.checked);
-            });
-        }
-
-        const language = this.getElement("settings-language");
-        if (language) {
-            language.addEventListener("change", event => {
-                SettingsManager.set("language", event.target.value);
-            });
-        }
-    }
-
-    registerEvents() {
-        eventBus.on("settings:update", () => this.render());
-    }
-
-    render() {
-        const settings = SettingsManager.getAll();
-
-        const numberFormat = this.getElement("settings-number-format");
-        const tickSpeed = this.getElement("settings-tick-speed");
-        const autoSave = this.getElement("settings-autosave");
-        const debugMode = this.getElement("settings-debug");
-        const speedRunMode = this.getElement("settings-speedrun");
-        const language = this.getElement("settings-language");
-
-        if (numberFormat) numberFormat.value = settings.numberFormat;
-        if (tickSpeed) tickSpeed.value = settings.tickSpeed;
-        if (autoSave) autoSave.value = settings.autoSaveInterval;
-        if (debugMode) debugMode.checked = Boolean(settings.debugMode);
-        if (speedRunMode) speedRunMode.checked = Boolean(settings.speedRunMode);
-        if (language) language.value = settings.language;
-    }
+class SettingsUI{
+ constructor(){this.initialized=false;}
+ initialize(){if(this.initialized)return;this.initialized=true;this.registerControls();this.registerEvents();this.render();}
+ getElement(id){return document.getElementById(id);}
+ registerControls(){
+  const n=this.getElement("settings-number-format");if(n)n.addEventListener("change",e=>SettingsManager.set("numberFormat",e.target.value));
+  const t=this.getElement("settings-tick-speed");if(t)t.addEventListener("change",e=>{if(SettingsManager.set("tickSpeed",e.target.value))Game.restart();else this.render();});
+  const a=this.getElement("settings-autosave");if(a)a.addEventListener("change",e=>{if(SettingsManager.set("autoSaveInterval",e.target.value))SaveManager.restartAutoSave();else this.render();});
+  const d=this.getElement("settings-debug");if(d)d.addEventListener("change",e=>SettingsManager.set("debugMode",e.target.checked));
+  const s=this.getElement("settings-speedrun");if(s)s.addEventListener("change",e=>SettingsManager.set("speedRunMode",e.target.checked));
+  const l=this.getElement("settings-language");if(l)l.addEventListener("change",e=>SettingsManager.set("language",e.target.value));
+ }
+ registerEvents(){eventBus.on("settings:update",()=>this.render());}
+ render(){const s=SettingsManager.getAll();const n=this.getElement("settings-number-format"),t=this.getElement("settings-tick-speed"),a=this.getElement("settings-autosave"),d=this.getElement("settings-debug"),sr=this.getElement("settings-speedrun"),l=this.getElement("settings-language");if(n)n.value=s.numberFormat;if(t)t.value=s.tickSpeed;if(a)a.value=s.autoSaveInterval;if(d)d.checked=Boolean(s.debugMode);if(sr)sr.checked=Boolean(s.speedRunMode);if(l)l.value=s.language;}
 }
-
 export default new SettingsUI();
