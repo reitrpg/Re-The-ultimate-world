c1e0ec40e84d100224e1614318602b56e1f2b8a6
reitrpg <rei.you2.11@gmail.com>
2026-09-24 10:19:45 +0900

    Fix World Creator public build and core integration

--- js/ui/SettingsUI.js
+++ js/ui/SettingsUI.js
@@ -6,6 +6,7 @@
 import eventBus from "../core/eventBus.js";
 import SettingsManager from "../settings/Manager.js";
 import Game from "../core/game.js";
+import SaveManager from "../core/save.js";
 
 class SettingsUI {
     constructor() {
@@ -29,24 +30,17 @@ class SettingsUI {
         const numberFormat = this.getElement("settings-number-format");
         if (numberFormat) {
             numberFormat.addEventListener("change", event => {
-                SettingsManager.set(
-                    "numberFormat",
-                    event.target.value
-                );
+                SettingsManager.set("numberFormat", event.target.value);
             });
         }
 
         const tickSpeed = this.getElement("settings-tick-speed");
         if (tickSpeed) {
             tickSpeed.addEventListener("change", event => {
-                const value = Number(event.target.value);
-
-                if (!Number.isFinite(value) || value <= 0) {
+                if (!SettingsManager.set("tickSpeed", event.target.value)) {
                     this.render();
                     return;
                 }
-
-                SettingsManager.set("tickSpeed", value);
                 Game.restart();
             });
         }
@@ -54,102 +48,56 @@ class SettingsUI {
         const autoSave = this.getElement("settings-autosave");
         if (autoSave) {
             autoSave.addEventListener("change", event => {
-                const value = Number(event.target.value);
-
-                if (!Number.isFinite(value) || value < 0) {
+                if (!SettingsManager.set("autoSaveInterval", event.target.value)) {
                     this.render();
                     return;
                 }
-
-                SettingsManager.set(
-                    "autoSaveInterval",
-                    value
-                );
+                SaveManager.restartAutoSave();
             });
         }
 
         const debugMode = this.getElement("settings-debug");
         if (debugMode) {
             debugMode.addEventListener("change", event => {
-                SettingsManager.set(
-                    "debugMode",
-                    event.target.checked
-                );
+                SettingsManager.set("debugMode", event.target.checked);
             });
         }
 
         const speedRunMode = this.getElement("settings-speedrun");
         if (speedRunMode) {
             speedRunMode.addEventListener("change", event => {
-                SettingsManager.set(
-                    "speedRunMode",
-                    event.target.checked
-                );
+                SettingsManager.set("speedRunMode", event.target.checked);
             });
         }
 
         const language = this.getElement("settings-language");
         if (language) {
             language.addEventListener("change", event => {
-                SettingsManager.set(
-                    "language",
-                    event.target.value
-                );
+                SettingsManager.set("language", event.target.value);
             });
         }
     }
 
     registerEvents() {
-        eventBus.on("settings:update", () => {
-            this.render();
-        });
+        eventBus.on("settings:update", () => this.render());
     }
 
     render() {
         const settings = SettingsManager.getAll();
 
-        const numberFormat = this.getElement(
-            "settings-number-format"
-        );
-        const tickSpeed = this.getElement(
-            "settings-tick-speed"
-        );
-        const autoSave = this.getElement(
-            "settings-autosave"
-        );
-        const debugMode = this.getElement(
-            "settings-debug"
-        );
-        const speedRunMode = this.getElement(
-            "settings-speedrun"
-        );
-        const language = this.getElement(
-            "settings-language"
-        );
+        const numberFormat = this.getElement("settings-number-format");
+        const tickSpeed = this.getElement("settings-tick-speed");
+        const autoSave = this.getElement("settings-autosave");
+        const debugMode = this.getElement("settings-debug");
+        const speedRunMode = this.getElement("settings-speedrun");
+        const language = this.getElement("settings-language");
 
-        if (numberFormat) {
-            numberFormat.value = settings.numberFormat;
-        }
-
-        if (tickSpeed) {
-            tickSpeed.value = settings.tickSpeed;
-        }
-
-        if (autoSave) {
-            autoSave.value = settings.autoSaveInterval;
-        }
-
-        if (debugMode) {
-            debugMode.checked = Boolean(settings.debugMode);
-        }
-
-        if (speedRunMode) {
-            speedRunMode.checked = Boolean(settings.speedRunMode);
-        }
-
-        if (language) {
-            language.value = settings.language;
-        }
+        if (numberFormat) numberFormat.value = settings.numberFormat;
+        if (tickSpeed) tickSpeed.value = settings.tickSpeed;
+        if (autoSave) autoSave.value = settings.autoSaveInterval;
+        if (debugMode) debugMode.checked = Boolean(settings.debugMode);
+        if (speedRunMode) speedRunMode.checked = Boolean(settings.speedRunMode);
+        if (language) language.value = settings.language;
     }
 }
 
