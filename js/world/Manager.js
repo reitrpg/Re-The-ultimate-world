cb87c7f628feebded0eb2cab4c522e9cddfb9e23
reitrpg <rei.you2.11@gmail.com>
2026-09-24 16:06:26 +0900

    Fix core input, tabs, world creation, and version

--- js/world/Manager.js
+++ js/world/Manager.js
@@ -1,12 +1,15 @@
 import World from "./World.js";
+import WorldGenerator from "./Generator.js";
 import eventBus from "../core/eventBus.js";
+
 class WorldManager{
  constructor(){this.worlds=[];this.activeWorldIndex=0;}
- create(seed){const w=new World(seed);this.worlds.push(w);eventBus.emit("world:update");return w;}
+ create(seed){const world=WorldGenerator.generate(seed||Date.now().toString());this.worlds.push(world);this.activeWorldIndex=this.worlds.length-1;eventBus.emit("world:update");return world;}
  get(index){return this.worlds[index];}
  getAll(){return this.worlds;}
  getCount(){return this.worlds.length;}
  getActive(){return this.worlds[this.activeWorldIndex];}
+ getActiveIndex(){return this.activeWorldIndex;}
  setActive(index){const n=Number(index);if(!Number.isInteger(n)||n<0||n>=this.worlds.length)return false;this.activeWorldIndex=n;eventBus.emit("world:update");return true;}
  update(deltaTime){const w=this.getActive();if(!w)return;w.update(deltaTime);eventBus.emit("world:update");}
  clear(){this.worlds=[];this.activeWorldIndex=0;eventBus.emit("world:update");}
