5ae0ad531aa598c8e915c695ff9b777127e5bdee
reitrpg <rei.you2.11@gmail.com>
2026-09-24 10:08:22 +0900

    Batch update World Creator UI integration

--- js/settings/Manager.js
+++ js/settings/Manager.js
@@ -5,174 +5,89 @@
 
 import eventBus from "../core/eventBus.js";
 
+const DEFAULT_SETTINGS = {
+    numberFormat: "scientific",
+    tickSpeed: 1000,
+    autoSaveInterval: 30000,
+    debugMode: false,
+    speedRunMode: false,
+    language: "ja"
+};
+
 class SettingsManager {
-
     constructor() {
-
-        this.settings = {
-
-            numberFormat:
-                "scientific",
-
-            tickSpeed:
-                1000,
-
-            autoSaveInterval:
-                30000,
-
-            debugMode:
-                false,
-
-            speedRunMode:
-                false,
-
-            language:
-                "ja"
-
-        };
-
+        this.settings = { ...DEFAULT_SETTINGS };
     }
 
     get(key) {
-
         return this.settings[key];
-
     }
 
-    set(
-        key,
-        value
-    ) {
+    getAll() {
+        return { ...this.settings };
+    }
 
-        if (
-
-            !Object.prototype.hasOwnProperty.call(
-
-                this.settings,
-
-                key
-
-            )
-
-        ) {
-
+    set(key, value) {
+        if (!Object.prototype.hasOwnProperty.call(this.settings, key)) {
             return false;
-
         }
 
-        this.settings[key] =
-            value;
+        this.settings[key] = value;
 
-        eventBus.emit(
-
-            "settings:update"
-
-        );
+        eventBus.emit("settings:update", {
+            key,
+            value
+        });
 
         return true;
-
     }
 
     getTickSpeed() {
-
-        return Number(
-
-            this.settings.tickSpeed
-
-        );
-
+        const value = Number(this.settings.tickSpeed);
+        return Number.isFinite(value) && value > 0
+            ? value
+            : DEFAULT_SETTINGS.tickSpeed;
     }
 
     getAutoSaveInterval() {
-
-        return Number(
-
-            this.settings.autoSaveInterval
-
-        );
-
+        const value = Number(this.settings.autoSaveInterval);
+        return Number.isFinite(value) && value >= 0
+            ? value
+            : DEFAULT_SETTINGS.autoSaveInterval;
     }
 
     isDebugMode() {
-
-        return Boolean(
-
-            this.settings.debugMode
-
-        );
-
+        return Boolean(this.settings.debugMode);
     }
 
     isSpeedRunMode() {
-
-        return Boolean(
-
-            this.settings.speedRunMode
-
-        );
-
+        return Boolean(this.settings.speedRunMode);
     }
 
     reset() {
+        this.settings = { ...DEFAULT_SETTINGS };
 
-        this.settings = {
-
-            numberFormat:
-                "scientific",
-
-            tickSpeed:
-                1000,
-
-            autoSaveInterval:
-                30000,
-
-            debugMode:
-                false,
-
-            speedRunMode:
-                false,
-
-            language:
-                "ja"
-
-        };
-
-        eventBus.emit(
-
-            "settings:update"
-
-        );
-
+        eventBus.emit("settings:update");
     }
 
     toJSON() {
-
-        return {
-
-            ...this.settings
-
-        };
-
+        return this.getAll();
     }
 
     load(data) {
-
-        if (!data) {
-
-            return;
-
+        if (!data || typeof data !== "object") {
+            return false;
         }
 
         this.settings = {
-
-            ...this.settings,
-
+            ...DEFAULT_SETTINGS,
             ...data
-
         };
 
-    }
+        eventBus.emit("settings:update");
 
+        return true;
+    }
 }
 
 export default new SettingsManager();
